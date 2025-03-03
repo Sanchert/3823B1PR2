dcea8bda7f476dc5bc112a3f724a9a45001bb04f
Sanchert <113541aert901@gmail.com>
2025-03-03 08:43:40 +0300

    deleted empty

--- lab3_postfix/KolotukhinAD/lab3_stack_lite/template/samples/main.cpp
+++ lab3_postfix/KolotukhinAD/lab3_stack_lite/template/samples/main.cpp
@@ -2,7 +2,6 @@
 #include "iostream"
 int main() {
     algexp expr("(1+2*3)*(8/1-3)");
-    // algexp expr("");
     int result = expr.solveIt();
     string a = expr.getExpr();
     cout << a << "    " << result;

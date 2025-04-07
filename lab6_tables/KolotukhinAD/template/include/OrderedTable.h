2bed41c066479303f3fe3f974b254fef16f720b7
Sanchert <113541aert901@gmail.com>
2025-04-07 10:18:20 +0300

    Ordered table: Vector

---
+++ lab6_tables/KolotukhinAD/template/include/OrderedTable.h
@@ -0,0 +1,364 @@
+#include <vector>
+#include "Polynomial.h"
+#include <iostream>
+
+using namespace std;
+
+template <typename TKey, typename TValue>
+class OrderedTable
+{
+private:
+    struct TableRec
+    {
+        TKey key;
+        TValue *value;
+    };
+    vector<TableRec> data {};
+public:
+
+    OrderedTable() {};
+
+    size_t GetSize() const { return data.size(); }
+
+    TValue& operator[](size_t pos) { return &(data[pos].value); }
+
+    void Delete(TKey _key)
+    {
+        int middle, left = 0, right = data.size();
+        while(left <= right)
+        {
+            middle = (left + right) / 2;
+            if ( _key < data[middle].key)
+                right = middle - 1;
+            else if (_key > data[middle].key)
+                left = middle + 1;
+            else 
+            {
+                for (int i = middle; i < data.size() - 1; i++)
+                {
+                    data[i] = data[i + 1];
+                }
+                data.pop_back();
+            }
+        }
+    }
+
+    TValue* Find(TKey _key)
+    {
+        int middle, left = 0, right = data.size();
+        while(left <= right)
+        {
+            middle = (left + right) / 2;
+            if ( _key < data[middle].key)
+                right = middle - 1;
+            else if (_key > data[middle].key)
+                left = middle + 1;
+            else 
+                return data[middle].value;
+        }
+        return nullptr;
+    }
+    
+    void Insert(TKey _key, TValue value)
+    {
+        if (data.size() == 0)
+        {
+            data.push_back({_key, new TValue(value)});
+        }
+        else if (!Find(_key))
+        {
+            for (int i = 0; i < data.size(); i++)
+            {
+                if (data[i].key > _key)
+                {
+                    data.push_back(data[data.size() - 1]);
+                    for (int j = data.size() - 1; j > i; j--)
+                    {
+                        data[j] = data[j - 1];
+                    }
+                    delete data[i];
+                    data[i] = TableRec{_key, new TValue(value)};
+                    return;
+                }
+            }
+            data.push_back({_key, new TValue(value)});
+        }
+    }
+
+    void Print()
+    {
+        for (TableRec val : data)
+        {
+            cout << val.key << ": " << *(val.value) << endl;
+        }
+    }
+};
+
+template <typename TKey>
+class PolynomialOrdTable
+{
+private:
+    struct TableRec
+    {
+        TKey key;
+        Polynomial polynomial;
+        double result;
+    };
+    std::vector<TableRec> data {};
+public:
+
+    PolynomialOrdTable() {};
+
+    size_t GetSize() const { return data.size(); }
+
+    Polynomial& operator[](size_t pos) { return data[pos].polynomial; }
+
+    void Delete(TKey _key)
+    {
+        int middle, left = 0, right = data.size();
+        while(left <= right)
+        {
+            middle = (left + right) / 2;
+            if ( _key < data[middle].key)
+                right = middle - 1;
+            else if (_key > data[middle].key)
+                left = middle + 1;
+            else 
+            {
+                for (int i = middle; i < data.size() - 1; i++)
+                {
+                    data[i] = data[i + 1];
+                }
+                data.pop_back();
+            }
+        }
+    }
+
+    Polynomial* Find(TKey _key)
+    {
+        int middle, left = 0, right = data.size();
+        while(left <= right)
+        {
+            middle = (left + right) / 2;
+            if ( _key < data[middle].key)
+                right = middle - 1;
+            else if (_key > data[middle].key)
+                left = middle + 1;
+            else 
+                return &data[middle].polynomial;
+        }
+        return nullptr;
+    }
+
+    void Insert(TKey _key, Polynomial _polynomial)
+    {
+        if (data.size() == 0)
+        {
+            data.push_back({_key, _polynomial, 0});
+        }
+        else if (!Find(_key))
+        {
+            for (int i = 0; i < data.size(); i++)
+            {
+                if (data[i].key > _key)
+                {
+                    data.push_back(data[data.size() - 1]);
+                    for (int j = data.size() - 1; j > i; j--)
+                    {
+                        data[j] = data[j - 1];
+                    }
+                    data[i] = TableRec{_key, _polynomial, 0};
+                    return;
+                }
+            }
+            data.push_back({_key, _polynomial, 0});
+        }
+    }
+
+    void Print()
+    {
+        for (int i = 0; i < data.size(); i++)
+        {
+            cout << data[i].key << ": " << data[i].polynomial << " = " << data[i].result << endl;
+        }
+    }
+
+    void Calculate(Point p)
+    {
+        for (auto& val : data)
+        {
+            val.result =  val.polynomial.calculate(p);
+        }
+    }
+};
+
+
+
+template <typename TKey>
+class PolynomialOrdTableArray
+{
+private:
+    struct TableRec
+    {
+        TKey key;
+        Polynomial polynomial;
+        double result;
+    };
+    int capacity;
+    int size;
+    TableRec* table;
+public:
+
+    PolynomialOrdTableArray() 
+    {
+        size = 0;
+        capacity = 2;
+        table = new TableRec[capacity];
+    }
+    
+    ~PolynomialOrdTableArray() 
+    {
+        delete[] table;
+    }
+
+    size_t GetSize() const { return size; }
+    
+    size_t GetCapacity() const { return capacity; }
+
+    Polynomial& operator[](size_t pos) 
+    {
+        if (pos < size)
+            return table[pos].polynomial;
+        throw out_of_range("Index out of range");
+    }
+
+    void Delete(TKey key)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (table[i].key == key)
+            {
+                table[i] = table[size - 1];
+                size--;
+                return;
+            }
+        }
+    }
+
+    Polynomial* Find(TKey key)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (table[i].key == key)
+            {
+                return &table[i].polynomial;
+            }
+        }
+        return nullptr;
+    }
+    
+    void Insert(TKey key, Polynomial p)
+    {
+        if (size == capacity)
+            resize();
+        if (Find(key))
+            return;
+        table[size] = {key, p};
+        size++;
+    }
+
+
+    void Print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << table[i].key << ": " << table[i].polynomial << " = " << table[i].result << endl;
+        }
+    }
+
+    void Calculate(Point p)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            table[i].result =  table[i].polynomial.calculate(p);
+        }
+    }
+
+    private: void resize()
+    {
+        capacity *= 2;
+        TableRec* newTable = new TableRec[capacity];
+        for (int i = 0; i < size; i++)
+        {
+            newTable[i] = table[i];
+        }
+        delete[] table;
+        table = newTable;
+    }
+};
+
+template <typename TKey>
+class PolynomialOrdTableList
+{
+private:
+    struct TableRec
+    {
+        TKey key;
+        Polynomial polynomial;
+        double result;
+    };
+    List<TableRec> table {};
+public:
+    PolynomialOrdTableList() {};
+
+    size_t GetSize() const { return table.getSize(); }
+
+    Polynomial& operator[](size_t pos) { return table[pos].polynomial; }
+
+    void Delete(TKey key)
+    {
+        for (int i = 0; i < table.getSize(); i++)
+        {
+            if (table[i].key == key)
+            {
+                table.removeFrom(i);
+                return;
+            }
+        }
+    }
+
+    Polynomial* Find(TKey key)
+    {
+        for (int i = 0; i < table.getSize(); i++)
+        {
+            if (table[i].key == key)
+            {
+                return &table[i].polynomial;
+            }
+        }
+        return nullptr;
+    }
+    
+    void Insert(TKey key, Polynomial polynomial)
+    {
+        if (Find(key))
+            return;
+        table.pushBack({key, polynomial});
+    }
+
+    void Print()
+    {
+        for (int i = 0; i < table.getSize(); i++)
+        {
+            cout << table[i].key << ": " << table[i].polynomial << " = " << table[i].result << endl;
+        }
+    }
+
+    void Calculate(Point p)
+    {
+        for (int i = 0; i < table.getSize(); i++)
+        {
+            table[i].result =  table[i].polynomial.calculate(p);
+        }
+    }
+};
--- lab6_tables/KolotukhinAD/template/samples/main.cpp
+++ lab6_tables/KolotukhinAD/template/samples/main.cpp
@@ -1,3 +1,4 @@
+#include "OrderedTable.h"
 #include "BinSTree.h"
 #include "UnorderedTable.h"
 #include "string"
@@ -6,15 +7,18 @@ using namespace std;
 
 int main()
 {
+    /*
     BinSTree<int, string> tree;
     tree.Insert(4, "top lv1");
     tree.Insert(2, "L lv2");
     tree.Insert(6, "R lv2");    
-    // tree.Insert(1, "LL lv3");
-    // tree.Insert(3, "LR lv3");
-    // tree.Insert(5, "RL lv3");
-    // tree.Insert(7, "RR lv3");
+    tree.Insert(1, "LL lv3");
+    tree.Insert(3, "LR lv3");
+    tree.Insert(5, "RL lv3");
+    tree.Insert(7, "RR lv3");
+    tree.Delete(6);
     tree.Print();
+    */
 
     /*    4
         2 | 6
@@ -38,10 +42,20 @@ int main()
     //______________Unordered Table [List]________________________________________________
     /*
     UnorderedTableList<int, string> T;
-    T.Insert(1, "first line");
-    T.Insert(2, "second line");
-    T.Insert(3, "third line");
+    T.Insert(3, "3 line");
+    T.Insert(2, "2 line");
+    T.Insert(1, "1 line");
+    
+    T.Insert(11, "6 line");
+    T.Insert(6, "4 line");
+    T.Insert(17, "8 line");
+    T.Insert(0, "0 line");
+    T.Insert(12, "7 line");
+    T.Insert(333, "9 line");
+    T.Insert(7, "5 line");
 
+    T.Delete(7);
+    cout << T.GetSize() << endl;
     T.Print();
     T.Delete(2);
     T.Print();
@@ -69,18 +83,19 @@ int main()
     //____________________________________________________________________________________
 
 
-    //______________Unordered Table Of Polynomials[Array, List, Vector]___________________
-    /*
+    //__________Unordered/Ordered Table Of Polynomials[Array, List, Vector]_______________
+    
     // PolynomialUTable<int> T;
     // PolynomialUTableArray<int> T;
-    PolynomialUTableList<int> T;
+    // PolynomialUTableList<int> T;
+    PolynomialOrdTable<int> T;
     Monom m1[] = { Monom(1,'x',2), Monom(3,'y',4), Monom(5,'z',6) };
     Polynomial p1(m1, sizeof(m1) / sizeof(Monom));
-    T.Insert(1, p1);
+    T.Insert(2, p1);
 
     Monom m2[] = { Monom(-1,'x',2), Monom(2,'y',2), Monom(-10,'z',2) };
     Polynomial p2(m2, sizeof(m2) / sizeof(Monom));
-    T.Insert(2, p2);
+    T.Insert(1, p2);
     
     Monom m3[] = { Monom(-13,'x',1), Monom(-2,'y',3), Monom(7,'z',5) };
     Polynomial p3(m3, sizeof(m3) / sizeof(Monom));
@@ -96,6 +111,7 @@ int main()
     T.Print();
     cout << endl;
     cout << *(T.Find(3)) << endl;
-    */
-   //_____________________________________________________________________________________
+    //_____________________________________________________________________________________
+
+
 }
